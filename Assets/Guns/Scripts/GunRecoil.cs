06c291d0e8b1890c4991a5141368e822123f4afc
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2021-10-10 17:53:28 +0530

    Fixed Gun Recoil

--- Assets/Guns/Scripts/GunRecoil.cs
+++ Assets/Guns/Scripts/GunRecoil.cs
@@ -3,53 +3,10 @@ using System.Collections;
 
 public class GunRecoil : MonoBehaviour
 {
-    [SerializeField] private float moveBack = .01f;
-    [SerializeField] private float speed = 10f;
-    [SerializeField] private float muzzleImpactDelay;
-
-    private Vector3 newPos;
-    private Vector3 startPos;
-    private float timeElapsed;
-
-    private void Start()
-    {
-        newPos = startPos = transform.localPosition;
-    }
-
-    private void Update()
-    {
-        // transform.localPosition = Vector3.Slerp(transform.localPosition, newPos, speed * Time.deltaTime);
-        //
-        // // if (1>2)
-        // // {
-        // //     newPos = startPos + Vector3.right * moveBack;
-        // //     StartCoroutine(Delay());
-        // // }
-    }
-
     public void Recoil()
     {
-        newPos = startPos + Vector3.right * moveBack;
-        StartCoroutine(Delay());
-        StartCoroutine(RecoilSlerpCoroutine());
+        GetComponent<Animator>().Play("Recoil", -1, 0f);
     }
 
-    private IEnumerator Delay()
-    {
-        yield return new WaitForSeconds(muzzleImpactDelay);
-        newPos = startPos;
-    }
-
-    private IEnumerator RecoilSlerpCoroutine()
-    {
-        timeElapsed = 0;
-        float totalTime = speed * Time.deltaTime;
-        if (timeElapsed < totalTime)
-        {
-            transform.localPosition = Vector3.Slerp(transform.localPosition, newPos, speed * Time.deltaTime);
-            timeElapsed += Time.deltaTime;
-            yield return null;
-        }
-    }
 
 }
